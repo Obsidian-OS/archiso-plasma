f42729d4cbf518aa06fb25c0b0d522070bc4d60a
/home/odd <odd@obsidianos.xyz>
2025-10-10 10:23:07 -0400

    Update profiledef.sh

--- profiledef.sh
+++ profiledef.sh
@@ -8,7 +8,7 @@ iso_application="ObsidianOS Live ISO (KDE)"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('uefi-ia32.systemd-boot.esp' 'uefi-x64.systemd-boot.esp' 'uefi-ia32.systemd-boot.eltorito' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('uefi.systemd-boot')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
